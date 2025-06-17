41dc9e4560375217ba56e56be10efadb256826ce
Rizky Fadillah <fadillahrizki16@gmail.com>
2025-06-17 13:45:13 +0700

    add analytics

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
+import { Analytics } from "@vercel/analytics/react"
 
 function Layout() {
   return (
     <>
+      <Analytics/>
       <Header/>
       <div className="py-16 bg-white dark:bg-gray-900 min-h-screen">
         <Outlet/>
